7b251b1caf6c43e1273322927f9ac1d9b32e89c9
Christian Alfoni <christianalfoni@gmail.com>
2020-04-30 12:32:00 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello There!</h1>
+      <h2>Start editing to see some magic mip!</h2>
+    </div>
+  );
+}

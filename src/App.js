09398007a960258ee44bcb373661f972dc2de307
Christian Alfoni <christianalfoni@gmail.com>
2020-05-07 17:47:09 +0200

    Again!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Christian</h1>
+      <h1>Hello Ives!</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );

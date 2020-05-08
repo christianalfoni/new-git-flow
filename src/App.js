b02a3709ac5765da9a509617bcc5b8cee0b8afa0
Christian Alfoni <christianalfoni@gmail.com>
2020-05-08 07:37:23 +0200

    Single title

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Mip Mop!</h1>
-      <h6>balp</h6>
-      <h4>Hmmm</h4>
+      <h1>Hello DemoTime!</h1>
     </div>
   );
 }

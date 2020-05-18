0764b917b7edf59f31c880424d22c220fb1c12c3
Christian Alfoni <christianalfoni@gmail.com>
2020-05-18 14:21:22 +0200

    Update from master

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello, I want this title blheipheiphe!</h1>
+      <h1>Hello, I updated it!</h1>
     </div>
   );
 }

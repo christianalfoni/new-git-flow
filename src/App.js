73598d38085738a0eb58efe83c4f3e87dd470b2b
Christian Alfoni <christianalfoni@gmail.com>
2020-06-10 11:45:17 +0200

    Cool!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello, Gitglflow, lets see if we can you!</h1>
+      <h1>Hello, Git!</h1>
     </div>
   );
 }

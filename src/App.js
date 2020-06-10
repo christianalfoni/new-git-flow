3ccfb1dafbf929964b789ae0e816cb1da1b5545b
Christian Alfoni <christianalfoni@gmail.com>
2020-06-10 11:45:33 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello, Git!</h1>
+      <h1>Hello, Git, lets make a PR instead!</h1>
     </div>
   );
 }
